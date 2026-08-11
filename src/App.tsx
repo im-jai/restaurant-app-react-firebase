311402889e827188fbe9bb2920c8a6dce96576e4
Jay <jayaveerapandiyan.d@gmail.com>
2026-08-11 21:54:38 +0530

    File name fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import Header from "./components/Header/Header";
 import Hero from "./components/Hero/Hero";
-import Menu from "./components/Menu/menu";
+import Menu from "./components/Menu/Menu";
 
 function App() {
   return (
